8ad1c51725543510da39c7b39fcfc681f0fcc2eb
Dan Hilderbrand <16111574+danh955@users.noreply.github.com>
2022-01-21 15:15:21 +0900

    YahooPricesResult

--- Z016.YahooFinanceApi/YahooClient.Prices.cs
+++ Z016.YahooFinanceApi/YahooClient.Prices.cs
@@ -20,15 +20,13 @@ public partial class YahooClient
     /// <param name="interval">Stock price interval.</param>
     /// <param name="cancellationToken">CancellationToken.</param>
     /// <returns>Task.</returns>
-    public async Task GetPrices(
+    public async Task<YahooPricesResult> GetPrices(
         string symbol,
         DateOnly? firstDate = null,
         DateOnly? lastDate = null,
         YahooInterval interval = YahooInterval.Daily,
         CancellationToken cancellationToken = default)
     {
-        this.logger?.LogInformation("{symbol}", symbol);
-
         string period1 = firstDate.HasValue ? firstDate.Value.ToUnixTimestamp() : Constant.EpochString;
         string period2 = lastDate.HasValue ? lastDate.Value.ToUnixTimestamp() : DateTime.Today.ToUnixTimestamp();
         string intervalString = ToIntervalString(interval);
@@ -36,6 +34,11 @@ public partial class YahooClient
         await this.CheckCrumb(cancellationToken);
         var url = $"https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={period1}&period2={period2}&interval={intervalString}&events=history&includeAdjustedClose=true&crumb={this.crumb}";
 
+        this.logger?.LogInformation("{symbol}", symbol);
         var response = await this.apiHttpClient.GetAsync(url, cancellationToken).ConfigureAwait(false);
+
+        return response.IsSuccessStatusCode
+            ? new YahooPricesResult(response)
+            : new YahooPricesResult(response.StatusCode);
     }
 }
--- Z016.YahooFinanceApi/YahooPricesResult.cs
+++ Z016.YahooFinanceApi/YahooPricesResult.cs
@@ -4,9 +4,59 @@
 
 namespace Z016.YahooFinanceApi;
 
+using System.Net;
+
 /// <summary>
 /// Yahoo prices result class.
 /// </summary>
 public class YahooPricesResult
 {
+    private readonly HttpResponseMessage? response;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="YahooPricesResult"/> class.
+    /// Successful constructor.
+    /// </summary>
+    /// <param name="response">HttpResponseMessage.</param>
+    internal YahooPricesResult(HttpResponseMessage response)
+    {
+        this.response = response;
+        this.IsSuccessful = true;
+        this.StatusCode = response.StatusCode;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="YahooPricesResult"/> class.
+    /// Unsuccessful constructor.
+    /// </summary>
+    /// <param name="statusCode">HttpStatusCode.</param>
+    internal YahooPricesResult(HttpStatusCode statusCode)
+    {
+        this.response = null;
+        this.IsSuccessful = false;
+        this.StatusCode = statusCode;
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether successful.
+    /// </summary>
+    public bool IsSuccessful { get; private set; }
+
+    /// <summary>
+    /// Gets the contains value of status codes defined for HTTP.
+    /// </summary>
+    public HttpStatusCode StatusCode { get; private set; }
 }
+
+/// <summary>
+/// A stock price record from the client.
+/// </summary>
+/// <param name="Date">Date of price.</param>
+/// <param name="Open">Opening price of the stock.</param>
+/// <param name="High">The high price.</param>
+/// <param name="Low">The low price.</param>
+/// <param name="Close">Closing price.</param>
+/// <param name="AdjClose">The adjusted closing price.</param>
+/// <param name="Volume">The volume traded.</param>
+
+public record YahooPrice(DateOnly Date, double? Open, double? High, double? Low, double? Close, double? AdjClose, long? Volume);
